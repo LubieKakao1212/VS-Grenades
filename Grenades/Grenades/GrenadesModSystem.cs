4523fa7df792f75694b2433904de7be77d249159
LubieKakao1212 <grzesio.go@gmail.com>
2025-02-08 12:45:11 +0100

    Project Setup

---
+++ Grenades/Grenades/GrenadesModSystem.cs
@@ -0,0 +1,11 @@
+﻿using Vintagestory.API.Common;
+
+namespace Grenades;
+
+public class GrenadesModSystem : ModSystem {
+    
+    public override void Start(ICoreAPI api) {
+        base.Start(api);
+    }
+    
+}
